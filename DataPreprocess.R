98596bf7503f2260509b8406580c5254c172aa90
RasmusMunkner <101603074+RasmusMunkner@users.noreply.github.com>
2023-03-16 09:34:00 +0100

    Appropriation of data preprocessing pipeline from the previous project to this one.

---
+++ DataPreprocess.R
@@ -0,0 +1,41 @@
+##### 1. Load packages and data ######
+library(dplyr)
+library(tidyverse)
+library(mlr3)
+library(lubridate)
+source("http://pcwww.liv.ac.uk/~william/R/crosstab.r")
+#install.packages(c("xts", "sp", "zoo"))
+#install.packages("CASdatasets", repos = "http://cas.uqam.ca/pub/", type="source")
+#install.packages('gmodels')
+library(gmodels)
+library(sp)
+library(xts)
+library(zoo)
+library(forcats)
+library(CASdatasets)
+library(fastDummies)
+source("Rasmus_Funktioner.R")
+
+#Load the data into memory
+data("freMPL1")
+
+ProcessedData <- freMPL1 %>% 
+  select(-c(RecordBeg, RecordEnd, ClaimInd)) %>% 
+  mutate(VehEnergy = fct_collapse(VehEnergy,
+                                  regular=c("regular", "eletric", "GPL")),
+         VehEngine = fct_collapse(VehEngine,
+                                  injection=c("injection", "electric", "GPL")),
+         HasKmLimit = factor(HasKmLimit)) %>% 
+  TreeModelGrouping("SocioCateg", "ClaimAmount", maxdepth = 2) %>% 
+  TreeModelGrouping("VehAge", "ClaimAmount", maxdepth = 2) %>% 
+  TreeModelGrouping("VehMaxSpeed", "ClaimAmount", maxdepth = 2) %>% 
+  TreeModelGrouping("VehPrice", "ClaimAmount", maxdepth = 2) %>% 
+  TreeModelGrouping("VehClass", "ClaimAmount", maxdepth = 2) %>% 
+  TreeModelGrouping("RiskVar", "ClaimAmount", maxdepth = 2) %>% 
+  dummy_cols(remove_selected_columns = T, remove_first_dummy = T)
+  
+
+#Writing the dataset to .csv files
+source("Rasmus_Funktioner.R")
+WriteData(ProcessedData, "processed_df")
+
---
+++ Rasmus_Funktioner.R
@@ -0,0 +1,96 @@
+
+#Grupperer en kategorisk variabel ved brug af en træ-model.
+#.data - Datasættet
+#.feature - Den kategoriske variabel, som skal grupperes
+#.target - Den variabel, som den kategoriske variabel skal prædiktere
+#predName - Navnet på kolonnen, som indeholder de prædikterede værdier for .target baseret på modellen
+#featureName - Navnet på kolonnen med den grupperede version af den kategoriske variabel - OBS: Den nye variabel kan ikke have samme navn som den gamle
+TreeModelGrouping <- function(.data, .feature, .target,
+                              pred = F, predName = "Pred", featureName = NULL, level_prefix = "Group_", maxdepth = 2, cp = 0){
+  
+  #Default er, at erstatte den ugrupperede variabel
+  if(is.null(featureName)){
+    featureName <- .feature
+  }
+  
+  #Opretter ML task
+  Task <- .data %>% 
+    dplyr::select(.feature, all_of(.target)) %>% 
+    as_task_regr(target = .target)
+  
+  #Opretter en træ-model, træner den og genererer prædiktioner
+  TreeClassifier <- lrn("regr.rpart", maxdepth = maxdepth, cp = cp)
+  TreeClassifier$train(Task)
+  Pred <- TreeClassifier$predict(Task)
+  
+  #Sæt den grupperede faktor på output-datasættet
+  if(.feature == featureName){
+    .data <- .data %>% select(-.feature)
+  }
+  
+  augmented_data <- 
+  .data %>% 
+    cbind(data.frame(
+      .pred = Pred$response,
+      .featureName = Pred$response %>% factor(labels = 1:length(unique(.)) %>% map_chr(.f=function(id){paste0(level_prefix,id)}))
+    ) %>%
+      setNames(c(predName, featureName))
+    )
+  
+  if(pred == F){
+    augmented_data %>% 
+      select(-all_of(predName)) %>% 
+      return()
+  } else {
+    augmented_data %>% 
+      return()
+  }
+  
+}
+
+#Ensure that variables are of the correct type
+# freMPL1 <- freMPL1 %>% 
+#   mutate(SocioCateg = SocioCateg %>%
+#            as.character() %>% 
+#            map_chr(.f = substr, start = 4, stop = 999) %>% 
+#            map_dbl(.f = as.numeric) %>% 
+#            factor(levels = 1:100),
+#          HasKmLimit = factor(HasKmLimit),
+#          RiskVar = factor(RiskVar),
+#          VehAge = fct_relevel(VehAge, "10+", after = 8))
+# 
+# freMPL1 %>% str()
+
+# colnames(freMPL1 %>% select(-ClaimInd, -ClaimAmount)) %>% 
+#   map(.f = function(feature){
+#     
+#     if(is.factor(freMPL1[[feature]])){
+#       print(feature)
+#       freMPL1 %>% 
+#         group_by(!!rlang::sym(feature)) %>% 
+#         summarise(Frek = mean(ClaimInd)) %>% 
+#         ggplot(aes(x = !!rlang::sym(feature), y = Frek)) +
+#         geom_point() %>% return()
+#     }
+#     
+#   })
+
+
+#Convenience functions for saving and reading data
+#df is the dataset to be saved
+#name is the name of the dataset, e.g. it is saved as 'name.csv'
+#Hence valid input for name is "DataFreq", not "DataFreq.csv", since the .csv is automatically added.
+WriteData <- function(df, name){
+  write_csv(df, paste0(name, ".csv"))
+  df %>%
+    map_dfr(.f=pillar::type_sum) %>% 
+    write_csv(file = paste0(name, "_coltypes.csv"))
+}
+
+ReadData <- function(name){
+  read_csv(paste0(name, ".csv"),
+           col_types = read_csv(paste0(name, "_coltypes.csv"), show_col_types = F) %>%
+             unlist() %>%
+             map_chr(.f=substr, 0,1)
+           )
+}
