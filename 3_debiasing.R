71fc55a544da8f2748b632a5a4837aec9d5c711e
Rasmus Munkner <rasmusvm1811@gmail.com>
2023-03-25 14:18:53 +0100

    Func debias in progress

--- 3_debiasing.R
+++ 3_debiasing.R
@@ -8,18 +8,17 @@ source("Rasmus_Funktioner.R")
 Data <- ReadData("processed_df") %>% 
   mutate(RowNr = row_number()) %>% 
   distinct(across(-RowNr), .keep_all = T) %>% 
-  add_column(GoodPredictor = rbinom(nrow(.), 1, prob = 0.5)) %>% 
-  mutate(ClaimAmountModified = 2 * ClaimAmount * (GoodPredictor == 1) + ClaimAmount * (GoodPredictor = 0))
+  mutate(ClaimAmountMod = 2 * ClaimAmount * (Gender == 2) + 1 * ClaimAmount * (Gender = 1))
 
 ####################################################
 #Set up tasks
 Task <- Data %>%
-  select(-ClaimAmountModified, -RowNr) %>% 
+  select(-ClaimAmountMod, -RowNr) %>% 
   as_task_regr(target = "ClaimAmount")
 
 TaskModified <- Data %>%
   select(-ClaimAmount, -RowNr) %>% 
-  as_task_regr(target = "ClaimAmountModified")
+  as_task_regr(target = "ClaimAmountMod")
 
 #####################################################
 #Set up models
@@ -32,27 +31,66 @@ XgbLearnerModified$param_set$values <- readRDS("hyperparameter_tuning_result_GP.
 XgbLearnerModified$train(TaskModified)
 
 #############################################
-#Predict
+#Causality-based debias
 AugmentedPredictionData <- Data %>% 
-  add_column(BiasPredict = XgbLearner$predict_newdata(.)$response) %>% 
+  add_column(Prediction_Biased = XgbLearner$predict_newdata(.)$response) %>% 
   select(-Gender) %>% 
   dplyr::cross_join(Data %>% 
                       group_by(Gender) %>% 
                       summarise(Prob = n() / nrow(.))) %>% 
-  add_column(Predictions = XgbLearner$predict_newdata(.)$response) %>% 
+  add_column(Prediction_Debiased = XgbLearner$predict_newdata(.)$response) %>% 
   group_by(RowNr) %>% 
-  summarise(Predicted = sum(Predictions * Prob), BiasedPredict = mean(BiasPredict))
+  summarise(Prediction_Debiased = sum(Prediction_Debiased * Prob), Prediction_Biased = mean(Prediction_Biased))
 
 AugmentedPredictionDataModified <- Data %>% 
-  add_column(BiasPredict = XgbLearnerModified$predict_newdata(.)$response) %>% 
-  mutate(OriginalGoodPredictorValue = GoodPredictor) %>% 
-  select(-GoodPredictor) %>% 
+  add_column(Prediction_Biased = XgbLearnerModified$predict_newdata(.)$response) %>% 
+  mutate(OriginalGender = Gender) %>% 
+  select(-Gender) %>% 
   dplyr::cross_join(Data %>% 
-                      group_by(GoodPredictor) %>% 
+                      group_by(Gender) %>% 
                       summarise(Prob = n() / nrow(.))) %>% 
-  add_column(Predictions = XgbLearnerModified$predict_newdata(.)$response) %>% 
-  group_by(RowNr, OriginalGoodPredictorValue) %>% 
-  summarise(Predicted = sum(Predictions * Prob), BiasedPredict = mean(BiasPredict))
+  add_column(Prediction_Debiased = XgbLearnerModified$predict_newdata(.)$response) %>% 
+  group_by(RowNr, OriginalGender) %>% 
+  summarise(Prediction_Debiased = sum(Prediction_Debiased * Prob), Prediction_Biased = mean(Prediction_Biased))
+
+#########################################################
+#Functional-decomposition debiasing
+
+Debiasframe <- function(df, colKey = "Gender"){
+  for (name in colnames(df)){
+    if(str_detect(name, colKey)){
+      nameSymb <- enquo(name)
+      df <- df %>% mutate(!!nameSymb := 0)
+    }
+  }
+  return(df)
+}
+
+#Fit a native Xgboost
+XgboostModel <- xgboost(data=Data %>% select(-ClaimAmountMod, -RowNr, -ClaimAmount) %>% as.matrix(),
+                        label=Data %>% select(ClaimAmount) %>% unlist(),
+                        nrounds=XgbLearner$param_set$values$nrounds,
+                        params=list(max_depth=XgbLearner$param_set$values$max_depth,
+                                    eta=XgbLearner$param_set$values$eta),
+                        monotone_constraint = c(Exposure = 1)
+)
+
+#Apply the glex function
+glex_xgb <- glex(XgboostModel, Data %>% select(-ClaimAmountMod, -RowNr, -ClaimAmount) %>% as.matrix()) 
+
+XgboostModelModified <- xgboost(data=Data %>% select(-ClaimAmountMod, -RowNr, -ClaimAmount) %>% as.matrix(),
+                        label=Data %>% select(ClaimAmountMod) %>% unlist(),
+                        nrounds=XgbLearnerModified$param_set$values$nrounds,
+                        params=list(max_depth=XgbLearnerModified$param_set$values$max_depth,
+                                    eta=XgbLearnerModified$param_set$values$eta),
+                        monotone_constraint = c(Exposure = 1)
+)
+
+glex_xgb_mod <- glex(XgboostModelModified, Data %>% select(-ClaimAmountMod, -RowNr, -ClaimAmount) %>% as.matrix()) 
+
+glex_xgb_mod$m <- glex_xgb_mod$m %>% Debiasframe()
+
+glex_vi(glex_xgb_mod) %>% autoplot(threshold = 5)
 
 ##########################################################
 #Tables
@@ -66,16 +104,18 @@ AugmentedPredictionDataModified %>%
 ##########################################################
 #Graphs
 AugmentedPredictionData %>% 
-  pivot_longer(names_to = "Predictor", cols = c("Predicted", "BiasedPredict")) %>% 
+  pivot_longer(names_to = "Predictor", cols = c("Prediction_Biased", "Prediction_Debiased")) %>% 
   ggplot() +
   geom_density(aes(x = value, color = Predictor)) +
+  scale_x_log10() +
   xlab("Estimate") +
   ylab("Density")
 
 AugmentedPredictionDataModified %>% 
-  pivot_longer(names_to = "Predictor", cols = c("Predicted", "BiasedPredict")) %>% 
+  pivot_longer(names_to = "Predictor", cols = c("Prediction_Biased", "Prediction_Debiased")) %>% 
   ggplot() +
-  geom_density(aes(x = value, color = Predictor)) +
+  geom_density(aes(x = pmax(30, value), color = Predictor)) +
+  scale_x_log10() +
   xlab("Estimate") +
   ylab("Density")
 
